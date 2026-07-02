b4c6ef267c04d7c753ce6759dfc622499706ff2f
Bram Vandenbogaerde <bram@vdb.space>
2026-07-02 19:57:19 +0200

    chore(scv): reduce logging

--- maf2-scv/src/Solver/Z3.hs
+++ maf2-scv/src/Solver/Z3.hs
@@ -179,12 +179,13 @@ instance {-# OVERLAPPING #-} (Show i, Ord i) => FormulaSolver i (Z3Solver i) whe
 
    solve _ (Atomic Fail) = return Unsat
    solve count script   = do
+      -- liftIO (print count)
       restoreCheckpoint
       -- Z3Solver $ liftIO $ putStrLn $ "trying to translate script " ++ show script
       -- Declare all variables as constants
       let (translatedScript, names, _) = translate count script
-      liftIO (putStrLn $ "Solver:original>>>" ++ show script)
-      liftIO (putStrLn $ "Solver:translated>>>" ++ show translatedScript)
+      -- liftIO (putStrLn $ "Solver:original>>>" ++ show script)
+      -- liftIO (putStrLn $ "Solver:translated>>>" ++ show translatedScript)
       -- evaluate the mall in the solver
       -- mapM_ (command . printf "(declare-const %s V)" ) names
       mapM_ (command . printf "(declare-const %s V)" ) names
